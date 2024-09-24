5b8464d6846cdeb126619dfc0f9e5890bef2d244
EditaGharibyan <editagharibyan.tt219-1@polytechnic.am>
2024-09-24 17:48:21 +0400

    Add files via upload

---
+++ gcd.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int a, b, gcd;//greatest common divisior
+    cout << "Enter two different numbers";
+    cin >> a >> b;
+    int a1 = a, b1 = b;
+    a = abs(a);
+    b = abs(b);
+    int min = a;
+    if (a > b) {
+        min = b;
+    }
+    for (int i = 1; i <= min; i++)
+    {
+        if (a % i == 0 && b % i == 0)
+        {
+            gcd = i;
+        }
+    }
+    cout << endl << "GCD= " << gcd;
+    int lcm = 1;//least common multiply
+    lcm = (a1 * b1) / gcd;
+    cout << endl << "Lcm = " << lcm;
+
+}
